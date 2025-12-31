dfe4bafa3790ff28d469fd5fd2199e48055d5f1e
Your Name <you@example.com>
2025-12-30 20:46:08 -0600

    quick customizations

--- movement_config.h
+++ movement_config.h
@@ -29,13 +29,10 @@
 
 const watch_face_t watch_faces[] = {
     clock_face,
-    world_clock_face,
+    stopwatch_face,
     sunrise_sunset_face,
     moon_phase_face,
-    stopwatch_face,
-    countdown_face,
-    alarm_face,
-    temperature_display_face,
+    tally_face,
     voltage_face,
     settings_face,
     set_time_face
@@ -49,7 +46,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 4)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 3)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
@@ -57,7 +54,7 @@ const watch_face_t watch_faces[] = {
 /* Determines the intensity of the led colors
  * Set a hex value 0-15 with 0x0 being off and 0xF being max intensity
  */
-#define MOVEMENT_DEFAULT_RED_COLOR 0x0
+#define MOVEMENT_DEFAULT_RED_COLOR 0x5
 #define MOVEMENT_DEFAULT_GREEN_COLOR 0xF
 #define MOVEMENT_DEFAULT_BLUE_COLOR 0x0
 
@@ -76,7 +73,7 @@ const watch_face_t watch_faces[] = {
  * 2: 5 minutes
  * 3: 30 minutes
  */
-#define MOVEMENT_DEFAULT_TIMEOUT_INTERVAL 0
+#define MOVEMENT_DEFAULT_TIMEOUT_INTERVAL 3
 
 /* Set the timeout before switching to low energy mode
  * Valid values are:
